d25a4b997f0214644d36021100cb42705573c2a8
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-01-30 22:42:55 +0100

    fixed canvas size

--- src/App.js
+++ src/App.js
@@ -3,39 +3,33 @@ import "./App.css";
 // import { createRoot } from "react-dom/client";
 // import React, { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
+// import { PerspectiveCamera } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 // import { Scene } from "three";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   const fbx = useLoader(FBXLoader, "/assets/model.fbx");
-  
+  const p0 = useLoader(FBXLoader, "/assets/p0.fbx");
+  const p1 = useLoader(FBXLoader, "/assets/p1.fbx");
+  const p2 = useLoader(FBXLoader, "/assets/p2.fbx");
+  const prop = useLoader(FBXLoader, "/assets/prop.fbx");
+
   return (
-    <div id="canvas-container">
-      
-      <Canvas>
-        <ambientLight intensity={0.1}></ambientLight>
-        <directionalLight color={"red"} position={[0, 0, 5]}></directionalLight>
-        <PerspectiveCamera
-          lookAt={[0,0,0]}
-          position = {[0,100,0]}
-          makeDefault
-          far={100}
-          near ={0.1}
-          fov = {35}
-          rotation = {[-Math.PI/2,0,0]}
-        />
-        
-        <primitive object={fbx} scale={1} />
-        
-        {/* <mesh>
-          <boxGeometry></boxGeometry>
-          <meshStandardMaterial></meshStandardMaterial>
-        </mesh> */}
-        
-      </Canvas>
-    </div>
+    <Canvas camera={{ position: [50, 50, 50], fov: 50 }}>
+      <ambientLight intensity={0.5} />
+
+      <directionalLight intensity={0.5} color={"white"} position={[0, 0, 5]} />
+
+      <primitive object={fbx} />
+      <primitive object={p0} />
+      <primitive object={p1} />
+      <primitive object={p2} />
+      <primitive object={prop} />
+
+      <OrbitControls />
+    </Canvas>
   );
 }
 
