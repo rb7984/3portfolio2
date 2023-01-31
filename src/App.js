93c4d5a30b3a35b4fb33ecda4639fcc8123ae3cb
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-01-31 23:55:08 +0100

    Perspective Camera

--- src/App.js
+++ src/App.js
@@ -2,44 +2,47 @@
 // import React, { useRef, useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import { useLoader, Canvas, } from "@react-three/fiber";
+import { useLoader, Canvas } from "@react-three/fiber";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
 function Model() {
-  
   const fbx = useLoader(FBXLoader, "/assets/model.fbx");
   const p0 = useLoader(FBXLoader, "/assets/p0.fbx");
   const p1 = useLoader(FBXLoader, "/assets/p1.fbx");
   const p2 = useLoader(FBXLoader, "/assets/p2.fbx");
   const prop = useLoader(FBXLoader, "/assets/prop.fbx");
-  
+
   return (
-  <>
-    <mesh receiveShadow = {true} castShadow = {true} >
-      
-      <primitive object={fbx} />
-      <primitive object={p0} />
-      <primitive object={p1} />
-      <primitive object={p2} />
-      <primitive object={prop} />
-      
-    </mesh>
-  </>
-  )
+    <>
+      <mesh>
+        <primitive object={fbx} />
+        <primitive object={p0} />
+        <primitive object={p1} />
+        <primitive object={p2} />
+        <primitive object={prop} />
+      </mesh>
+    </>
+  );
 }
 
 function App() {
-
   return (
-    <Canvas camera={{ position: [50, 50, 50], fov: 50 }} >
-      
+    <Canvas>
+      <PerspectiveCamera makeDefault position={[50, 50, 50]} fov={50} />
+
       <ambientLight castShadow intensity={0.2} />
-      
-      <spotLight castShadow shadowCameraFar={50} shadowMapHeight={2048} intensity={0.4} color={"white"} position={[0, 50, 0]} />
-      
+
+      <spotLight
+        castShadow
+        shadowMapHeight={2048}
+        intensity={0.4}
+        color={"white"}
+        position={[0, 50, 0]}
+      />
+
       <Model />
-      
+
       <OrbitControls />
     </Canvas>
   );
