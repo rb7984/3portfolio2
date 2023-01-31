361bfd5df1ecae3a3df7e34502aaa9153c0b2116
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-01-31 14:22:24 +0100

    separated model, no shadows

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-// import logo from "./logo.svg";
-import "./App.css";
 // import { createRoot } from "react-dom/client";
 // import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-// import { PerspectiveCamera } from "@react-three/drei";
-import { useLoader } from "@react-three/fiber";
+// import logo from "./logo.svg";
+import "./App.css";
+import { useLoader, Canvas, } from "@react-three/fiber";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-// import { Scene } from "three";
 import { OrbitControls } from "@react-three/drei";
 
-function App() {
+function Model() {
+  
   const fbx = useLoader(FBXLoader, "/assets/model.fbx");
   const p0 = useLoader(FBXLoader, "/assets/p0.fbx");
   const p1 = useLoader(FBXLoader, "/assets/p1.fbx");
   const p2 = useLoader(FBXLoader, "/assets/p2.fbx");
   const prop = useLoader(FBXLoader, "/assets/prop.fbx");
+  
+  return (
+  <>
+    <primitive object={fbx} />
+    <primitive object={p0} />
+    <primitive object={p1} />
+    <primitive object={p2} />
+    <primitive object={prop} />
+  </>
+  )
+}
+
+function App() {
 
   return (
-    <Canvas camera={{ position: [50, 50, 50], fov: 50 }}>
-      <ambientLight intensity={0.5} />
-
-      <directionalLight intensity={0.5} color={"white"} position={[0, 0, 5]} />
-
-      <primitive object={fbx} />
-      <primitive object={p0} />
-      <primitive object={p1} />
-      <primitive object={p2} />
-      <primitive object={prop} />
-
+    <Canvas camera={{ position: [50, 50, 50], fov: 50 }} >
+      
+      <ambientLight castShadow intensity={0.5} />
+      
+      <spotLight castShadow shadowMapHeight={2048} intensity={0.4} color={"white"} position={[0, 50, 0]} />
+      
+      <Model />
+      
       <OrbitControls />
     </Canvas>
   );
