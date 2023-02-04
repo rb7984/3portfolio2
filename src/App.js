266c527a24ef6beafbca8bc55152450500bd7411
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-02-04 22:37:18 +0100

    shadows

--- src/App.js
+++ src/App.js
@@ -10,59 +10,88 @@ import * as THREE from "three";
 function Model() {
   const fbxLoader = new FBXLoader();
 
-  const namesList = ["model", "p0", "p1", "p2", "prop"];
-  const groupsList = [];
+  var model = new THREE.Object3D();
+  model.castShadow = true;
+  model.receiveShadow = true;
 
-  const model = new THREE.Group();
-  const p0 = new THREE.Group();
-  const p1 = new THREE.Group();
-  const p2 = new THREE.Group();
-  const prop = new THREE.Group();
+  fbxLoader.load("/assets/model.fbx", (a) => {
+    a.traverse(function (node) {
+      if (node instanceof THREE.Mesh) {
+        const oldMat = node.material;
 
-  groupsList.push(model);
-  groupsList.push(p0);
-  groupsList.push(p1);
-  groupsList.push(p2);
-  groupsList.push(prop);
+        node.material = new THREE.MeshStandardMaterial({
+          color: oldMat.color,
+          map: oldMat.map,
+        });
 
-  for (let i = 0; i < namesList.length; i++) {
-    var path = "/assets/" + namesList[i] + ".fbx";
-
-    fbxLoader.load(path, (a) => {
-      a.traverse(function (node) {
-        if (node instanceof THREE.Mesh) {
-          node.castShadow = true;
-          node.receiveShadow = true;
-
-          const oldMat = node.material;
-
-          node.material = new THREE.MeshStandardMaterial({
-            color: oldMat.color,
-            map: oldMat.map,
-          });
-        }
-      });
-
-      groupsList[i].add(a);
+        node.castShadow = true;
+        node.receiveShadow = true;
+      }
     });
-  }
+    model.add(a);
+  });
 
   //   const fbx = useLoader(FBXLoader, "/assets/model.fbx");
 
-  return (
-    <>
-      <primitive object={groupsList[0]} castShadow receiveShadow />
-
-      {/* <primitive object={groupsList[1]} />
-        <primitive object={groupsList[2]} />
-        <primitive object={groupsList[3]} /> */}
-      {/* <primitive object={groupsList[4]} /> */}
-    </>
-  );
+  return <primitive object={model} castShadow receiveShadow />;
 }
 
+// function Model() {
+//   const fbxLoader = new FBXLoader();
+
+//   const namesList = ["model", "p0", "p1", "p2", "prop"];
+//   const groupsList = [];
+
+//   const model = new THREE.Group();
+//   const p0 = new THREE.Group();
+//   const p1 = new THREE.Group();
+//   const p2 = new THREE.Group();
+//   const prop = new THREE.Group();
+
+//   groupsList.push(model);
+//   groupsList.push(p0);
+//   groupsList.push(p1);
+//   groupsList.push(p2);
+//   groupsList.push(prop);
+
+//   for (let i = 0; i < namesList.length; i++) {
+//     var path = "/assets/" + namesList[i] + ".fbx";
+
+//     fbxLoader.load(path, (a) => {
+//       a.traverse(function (node) {
+//         if (node instanceof THREE.Mesh) {
+//           node.castShadow = true;
+//           node.receiveShadow = true;
+
+//           const oldMat = node.material;
+
+//           node.material = new THREE.MeshStandardMaterial({
+//             color: oldMat.color,
+//             map: oldMat.map,
+//           });
+//         }
+//       });
+
+//       groupsList[i].add(a);
+//     });
+//   }
+
+//   //   const fbx = useLoader(FBXLoader, "/assets/model.fbx");
+
+//   return (
+//     <>
+//       <primitive object={groupsList[0]} />
+
+//       {/* <primitive object={groupsList[1]} />
+//         <primitive object={groupsList[2]} />
+//         <primitive object={groupsList[3]} /> */}
+//       {/* <primitive object={groupsList[4]} /> */}
+//     </>
+//   );
+// }
+
 function Plane() {
-  var g2 = new THREE.PlaneBufferGeometry(2000, 2000, 8, 8);
+  var g2 = new THREE.PlaneGeometry(2000, 2000, 8, 8);
   var m2 = new THREE.MeshStandardMaterial({
     color: "#2c61d4",
     side: THREE.DoubleSide,
