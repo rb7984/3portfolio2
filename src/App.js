3c80b9c1289cffbaf50351bad7573e12477cc617
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-02-12 23:44:31 +0100

    shadows

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import {
-  OrbitControls,
-  PerspectiveCamera,
-  BakeShadows,
-} from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
-import { A2 } from "./A2";
+import { Model } from "./Model";
 
 function Plane() {
   var g2 = new THREE.PlaneGeometry(2000, 2000, 8, 8);
@@ -22,13 +18,19 @@ function Plane() {
 }
 
 function Light() {
-  var light = new THREE.DirectionalLight(0xc9e4ff, 0.3);
+  var light = new THREE.DirectionalLight(0xc9e4ff, 0.4);
 
-  light.position.set(30, 30, -30);
+  light.position.set(50, 50, -50);
   light.lookAt(0, 0, 0);
   light.castShadow = true;
-  light.shadow.mapSize.width = 2048;
-  light.shadow.mapSize.height = 2048;
+  light.shadow.mapSize.width = 4096;
+  light.shadow.mapSize.height = 4096;
+
+  let cameraDimension = 50;
+  light.shadow.camera.bottom = -cameraDimension;
+  light.shadow.camera.top = cameraDimension;
+  light.shadow.camera.left = -cameraDimension;
+  light.shadow.camera.right = cameraDimension;
   light.shadow.camera.far = 100;
 
   var lH = new THREE.DirectionalLightHelper(light, 2);
@@ -43,14 +45,13 @@ function Light() {
 
 function App() {
   return (
-    <Canvas shadows>
-      <PerspectiveCamera makeDefault position={[5, 5, 5]} fov={50} />
+    <Canvas shadows dpr={(1, 1)}>
+      <PerspectiveCamera makeDefault position={[50, 50, 50]} fov={50} />
 
-      <ambientLight intensity={1} />
+      <hemisphereLight intensity={0.6} />
       <Light />
 
-      <A2 />
-
+      <Model />
       <Plane />
       <OrbitControls />
     </Canvas>
@@ -58,118 +59,3 @@ function App() {
 }
 
 export default App;
-
-// function Model() {
-//   const fbx = useFBX("assets/model.fbx");
-
-//   fbx.castShadow = true;
-//   fbx.receiveShadow = true;
-
-//   fbx.traverse(function (node) {
-//     if (node instanceof THREE.Mesh) {
-//       node.castShadow = true;
-//       node.receiveShadow = true;
-
-//       // const oldMat = node.material;
-
-//       // node.material = new THREE.MeshLambertMaterial({
-//       //   color: oldMat.color,
-//       //   map: oldMat.map,
-//       // });
-//     }
-//   });
-
-//   return <primitive object={fbx} />;
-// }
-
-// function Model() {
-//   const fbx = useLoader(FBXLoader, "assets/model.fbx");
-
-//   const fff = useFBX("assets/model.fbx");
-
-//   fbx.children.forEach((object) => {
-//     object.castShadow = true;
-//     object.receiveShadow = true;
-//   });
-//   fbx.castShadow = true;
-//   fbx.receiveShadow = true;
-//   //   const fbx = useLoader(FBXLoader, "/assets/model.fbx");
-
-//   return (
-//     <mesh castShadow receiveShadow>
-//       <primitive object={fbx} />
-//     </mesh>
-//   );
-// }
-
-// function Try() {
-//   return (
-//     <mesh castShadow receiveShadow position={[0, 3, 0]}>
-//       <sphereGeometry />
-//       <meshStandardMaterial color={"red"} />
-//     </mesh>
-//   );
-// }
-
-// function Try2() {
-//   return (
-//     <mesh castShadow receiveShadow position={[0, 0, 0]}>
-//       <boxGeometry castShadow receiveShadow />
-//       <meshStandardMaterial color={"red"} />
-//     </mesh>
-//   );
-// }
-
-// function Model() {
-//   const fbxLoader = new FBXLoader();
-
-//   const namesList = ["model", "p0", "p1", "p2", "prop"];
-//   const groupsList = [];
-
-//   const model = new THREE.Group();
-//   const p0 = new THREE.Group();
-//   const p1 = new THREE.Group();
-//   const p2 = new THREE.Group();
-//   const prop = new THREE.Group();
-
-//   groupsList.push(model);
-//   groupsList.push(p0);
-//   groupsList.push(p1);
-//   groupsList.push(p2);
-//   groupsList.push(prop);
-
-//   for (let i = 0; i < namesList.length; i++) {
-//     var path = "/assets/" + namesList[i] + ".fbx";
-
-// fbxLoader.load(path, (a) => {
-//   a.traverse(function (node) {
-//     if (node instanceof THREE.Mesh) {
-//       node.castShadow = true;
-//       node.receiveShadow = true;
-
-//       const oldMat = node.material;
-
-//       node.material = new THREE.MeshStandardMaterial({
-//         color: oldMat.color,
-//         map: oldMat.map,
-//       });
-//     }
-//   });
-
-//   groupsList[i].add(a);
-// });
-//   }
-
-//   //   const fbx = useLoader(FBXLoader, "/assets/model.fbx");
-
-//   return (
-//     <>
-//       <primitive object={groupsList[0]} />
-
-//       {/* <primitive object={groupsList[1]} />
-//         <primitive object={groupsList[2]} />
-//         <primitive object={groupsList[3]} /> */}
-//       {/* <primitive object={groupsList[4]} /> */}
-//     </>
-//   );
-// }
