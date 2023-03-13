018eb12b517db7479fa855be23ea5b6045336e91
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-03-13 14:07:51 +0100

    added buttons

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./index.css"
 
 export function Banner() {
     return (
-      <p>
+      <div className="banner">
         Hi There! My Name is Riccardo Barelli, I am a Architecture and Civil
         Engineering graduate from Bologna. Here are some of my projects!
-      </p>
+      </div>
     );
 }
---
+++ src/BtnPrevious.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./index.css"
+
+export function BtnPrevious() {
+return (
+<div className="btnPrevious" id = 'previous'>
+<div className="arrow-left"></div>
+</div>
+)
+}
