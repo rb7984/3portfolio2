6274fb7d15e5e29565c4626b43012eeebc2c1a55
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-03-21 23:22:52 +0100

    prop

--- src/Prop.jsx
+++ src/Prop.jsx
@@ -2,22 +2,22 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 
-
-
 export function Prop(props) {
   const { nodes, materials } = useGLTF('/assets/gl/prop.glb')
   const ref = useRef();
 
   useFrame(({clock}) =>{
-    var a = clock.getElapsedTime();
-    ref.current.position.x = 2.75 * Math.sin(a) +3;
-    ref.current.position.y = 9.5 * Math.sin(a) + 8;
-    ref.current.position.z = 12.5 * Math.sin(a) + 11;
+    var a = clock.getElapsedTime()/2;
+    
+    ref.current.position.x = 2.5 * Math.sin(a) + 2.5;
+    ref.current.position.y = 8.5 * Math.sin(a) + 8.5;
+    ref.current.position.z = 11.5 * Math.sin(a) + 11.5;
   }
   )
 
   return (
     <group {...props} ref = {ref} dispose={null}>
+
       <group rotation={[Math.PI / 2, 0, 0]} scale={1}>
         <mesh castShadow receiveShadow geometry={nodes.Object_1.geometry} material={materials.Roof} />
         <mesh castShadow receiveShadow geometry={nodes.Object_2.geometry} material={materials.po_Roof} />
